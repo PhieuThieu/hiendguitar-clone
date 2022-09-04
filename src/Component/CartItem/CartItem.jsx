6dc4529a520c8516d332d3500302a10811be5dd4
PhieuThieu <phieu.yiu@gmail.com>
2022-09-04 16:06:15 +0700

    fixed aspect-ratio safari

--- src/Component/CartItem/CartItem.jsx
+++ src/Component/CartItem/CartItem.jsx
@@ -5,7 +5,7 @@ function CartItem({image, name, price, quantity, index, active}) {
 
   return (
     <div className=' grid grid-cols-4 mb-8'>
-      <img className='rounded-xl object-cover h-full aspect-square col-span-1' src={image} alt=""/>
+      <img className='rounded-xl object-cover h-auto aspect-square col-span-1' src={image} alt=""/>
       <ItemDescription
         index={index}
         name={name}

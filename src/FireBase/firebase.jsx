f6ed77bb9e947ac0d0df64f4cb17441890e1c1e8
PhieuThieu <phieu.yiu@gmail.com>
2022-09-19 09:30:59 +0700

    update firebass

--- src/FireBase/firebase.jsx
+++ src/FireBase/firebase.jsx
@@ -3,14 +3,14 @@ import {getAuth} from 'firebase/auth'
 import {getDatabase} from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHn2_J1vff4ogGIwLpQVvqoEKRGboIPHA",
-  authDomain: "hiendguitar-clone.firebaseapp.com",
-  projectId: "hiendguitar-clone",
-  storageBucket: "hiendguitar-clone.appspot.com",
-  messagingSenderId: "169174120351",
-  appId: "1:169174120351:web:e7b87cba4d6e172fc5efaa",
-  measurementId: "G-91DXY7HH73",
-  databaseURL: "https://hiendguitar-clone-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  apiKey: "AIzaSyB1lcZWHO_NShVXgpk37ser2vl5sUpfhqQ",
+  authDomain: "hiendguitar-68be4.firebaseapp.com",
+  databaseURL: "https://hiendguitar-68be4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hiendguitar-68be4",
+  storageBucket: "hiendguitar-68be4.appspot.com",
+  messagingSenderId: "395265263098",
+  appId: "1:395265263098:web:40a07d36de656bcd0fd043",
+  measurementId: "G-BF1CVVC438"
 };
 
 // Initialize Firebase
